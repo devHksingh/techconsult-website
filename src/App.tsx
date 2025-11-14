c0ae5781f373fbcb1b4a0b0c42f69fd3e49d9d21
devHksingh <hksingh.dev@gmail.com>
2025-11-14 11:15:36 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from '@/components/ui/moving-border';
+import { LayoutTextFlip } from "@/components/ui/layout-text-flip";
+import { motion } from "motion/react";
+import { HeroHighlight,Highlight } from './components/ui/hero-highlight'
+// import { HeroHighlight, Highlight } from "../ui/hero-highlight";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <motion.div className="relative mx-4 my-4 flex flex-col items-center justify-center gap-4 text-center sm:mx-0 sm:mb-0 sm:flex-row">
+        <LayoutTextFlip
+          text="Welcome to "
+          words={["Aceternity UI", "Fight Club", "The Matrix", "The Jungle"]}
+        />
+      </motion.div>
+      <p className="mt-4 text-center text-base text-neutral-600 dark:text-neutral-400">
+        Experience the power of modern UI components that bring your ideas to
+        life.
+      </p>
+    </div>
+       <HeroHighlight>
+      <motion.h1
+        initial={{
+          opacity: 0,
+          y: 20,
+        }}
+        animate={{
+          opacity: 1,
+          y: [20, -5, 0],
+        }}
+        transition={{
+          duration: 0.5,
+          ease: [0.4, 0.0, 0.2, 1],
+        }}
+        className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+      >
+        With insomnia, nothing&apos;s real. Everything is far away. Everything
+        is a{" "}
+        <Highlight className="text-black dark:text-white">
+          copy, of a copy, of a copy.
+        </Highlight>
+      </motion.h1>
+    </HeroHighlight>
+    <div>
+      <Button
+        borderRadius="1.75rem"
+        className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+      >
+        Borders are cool
+      </Button>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
